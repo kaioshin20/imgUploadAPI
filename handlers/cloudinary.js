26c351c9760a5e4e532e3f48a83a14962d2da72f
Aayush-1999 <aayushaggarwal2007@gmail.com>
2019-10-01 22:17:42 +0530

    Add environment variables

--- handlers/cloudinary.js
+++ handlers/cloudinary.js
@@ -1,6 +1,6 @@
 const cloudinary = require('cloudinary')
 cloudinary.config({
     cloud_name: 'kaioshin',
-    api_key: API_KEY,
-    api_secret: API_SECRET
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 })
