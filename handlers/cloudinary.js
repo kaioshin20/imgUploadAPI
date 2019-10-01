d3365bc6968cdd3517ea37fcbb74a315a829deb4
Rajat Cambo <rajatcambo7@gmail.com>
2019-10-01 18:25:21 +0530

    added code

---
+++ handlers/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary')
+cloudinary.config({
+    // cloud_name: process.end.CLOUD_NAME,
+    // api_key: process.env.API_ID,
+    // api_secret: process.env.API_SECRET
+    cloud_name: 'kaioshin',
+    api_key: '722974177559755',
+    api_secret: 'b_Qa_8k5iydI_0bi9qliEYKp9Os'
+})
--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const upload = require("./handlers/multer")
+require('dotenv').config()
+require('./handlers/cloudinary')
+const cloudinary = require('cloudinary')
 const app = express()
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
 app.get('/', (req, res) => {
-  res.render('index', {
-    blogs: [{ name: 'Hello' }, { name: 'World' }, { name: 'hi' }]
-  })
+  res.render('index')
+})
+
+app.post('/create_blog', upload.single("image"), async (req, res) => {
+  const result = await cloudinary.v2.uploader.upload(req.file.path)
+  res.send(result)
 })
 
 app.get('/about', (req, res) => {
