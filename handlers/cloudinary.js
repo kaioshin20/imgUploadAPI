b163f70d13ec1e02a198f7c019145bc123e836a7
Rajat Cambo <rajatcambo7@gmail.com>
2019-10-01 18:33:21 +0530

    code added

--- handlers/cloudinary.js
+++ handlers/cloudinary.js
@@ -1,9 +1,6 @@
 const cloudinary = require('cloudinary')
 cloudinary.config({
-    // cloud_name: process.end.CLOUD_NAME,
-    // api_key: process.env.API_ID,
-    // api_secret: process.env.API_SECRET
     cloud_name: 'kaioshin',
-    api_key: '722974177559755',
-    api_secret: 'b_Qa_8k5iydI_0bi9qliEYKp9Os'
+    api_key: API_KEY,
+    api_secret: API_SECRET
 })
